1c900712aff85748393022306cce04c923d1df51
naveen <naranaveen576@gmail.com>
2024-08-12 15:47:53 +0530

    mapstateprops-- ownprops

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import store from "./redux/store";
 import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 //npm i react-redux
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <NewCakeContainer />
+        <ItemContainer cakes/>
+        
       </div>
     </Provider>
   );
